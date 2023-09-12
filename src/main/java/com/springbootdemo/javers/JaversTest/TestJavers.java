c2701d4e4ffae46d5cdc391e6bce705be7ac9bc1
Raviranjan kumar <raviranjan.kumar@transporeon.com>
2023-09-12 17:43:18 +0530

    tree transverse code added

--- src/main/java/com/springbootdemo/javers/JaversTest/TestJavers.java
+++ src/main/java/com/springbootdemo/javers/JaversTest/TestJavers.java
@@ -10,11 +10,14 @@ import java.util.List;
 
 public class TestJavers {
     public static void main(String[] args) {
+
+        //***contains all kind of changes valueChanges ,Listchanges,Referecnce changes***///
+
         Student student1 = new Student();
         student1.setId(1);
         student1.setName("raviranjan");
         student1.setFavouriteSubject(Arrays.asList("math","science"));
-        student1.setTeachers(new Teacher(1,"mukesh"));
+        student1.setTeachers(null);
         student1.setTeacherList(Arrays.asList(new Teacher(1,"rj"),new Teacher(2,"rk")));
 
         Student student2 = new Student();
@@ -22,12 +25,11 @@ public class TestJavers {
         student2.setName("raviranjan");
         student2.setFavouriteSubject(Arrays.asList("math","sci","programming"));
         student2.setTeachers(new Teacher(1,"rukesh"));
-        student2.setTeacherList(Arrays.asList(new Teacher(1,"rj"),new Teacher(2,"rm")));
+        student2.setTeacherList(Arrays.asList(new Teacher(1,"rj"),new Teacher(2,"rm"),new Teacher(3,"shubham")));
 
         Javers javers = JaversBuilder.javers().build();
 
         Diff diff = javers.compare(student1,student2);
         List<ValueChange> change = diff.getChangesByType(ValueChange.class);
-
     }
 }
