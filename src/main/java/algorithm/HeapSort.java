dce8a2315cef2235fdc9f82dfaf4917a597f8a0c
Raviranjan kumar <raviranjan.kumar@transporeon.com>
2023-09-13 16:46:08 +0530

    complete code for heap sort added

--- src/main/java/algorithm/HeapSort.java
+++ src/main/java/algorithm/HeapSort.java
@@ -1,9 +1,14 @@
 package algorithm;
 
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+
+import java.util.Collections;
+import java.util.List;
+
 public class HeapSort {
     //Heap sort function
-    public void heapSort(int arr[]) {
-        int n = arr.length;
+    public void heapSort(List<Integer> arr) {
+        int n = arr.size();
 
         // Building a max-heap for each iteration of the loop
         for (int i = n / 2 - 1; i >= 0; i--) {
@@ -11,38 +16,53 @@ public class HeapSort {
         }
         // Heap sort
         for (int i = n - 1; i >= 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+            Collections.swap(arr,0,i);
 
             // Heapify root element recursively
             heapify(arr, i, 0);
         }
     }
 
-    void heapify(int arr[], int n, int index) {
+    void heapify(List<Integer> arr, int n, int index) {
         // Finding the left and the right child
         int largest = index;
         int left = 2 * index + 1;
         int right = 2 * index + 2;
 
 //Setting the largest out of root, left child & right child
-        if (left < n && arr[left] > arr[largest])
+        if (left < n && arr.get(left) > arr.get(right))
             largest = left;
 
-        if (right < n && arr[right] > arr[largest])
+        if (right < n && arr.get(right) > arr.get(largest))
             largest = right;
 
         // If index is not equal to largest
         if (largest != index) {
             //perform swap
-            int temp = arr[index];
-            arr[index] = arr[largest];
-            arr[largest] = temp;
+            Collections.swap(arr,index,largest);
 
             // Calling the heapify function recursively
             heapify(arr, n, largest);
         }
     }
+
+    public void insertNode(List<Integer> arr, int value) {
+        int size = arr.size();
+        arr.add(value);
+        int index = size;
+        size++;
+
+        while(index > 0) {
+            int parentIndex = (index-1)/2;
+
+            if(arr.get(index) > arr.get(parentIndex)) {
+                Collections.swap(arr,index,parentIndex);
+                index = parentIndex;
+            }else{
+                break;
+            }
+        }
+    }
+
 }
 
--- src/main/java/algorithm/HeapSortCaller.java
+++ src/main/java/algorithm/HeapSortCaller.java
@@ -1,13 +1,17 @@
 package algorithm;
 
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HeapSortCaller {
     public static void main(String[] args) {
-        int arr[] = {3,6,8,2,9,10,12};
+        List<Integer> arr =  new ArrayList<>(Arrays.asList(3,6,8,2,9,10,12));
         System.out.println("***Array before sorting*****");
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(arr[i] + " ");
+        for(int i=0;i<arr.size();i++) {
+            System.out.print(arr.get(i) + " ");
         }
         System.out.println();
 
@@ -15,8 +19,21 @@ public class HeapSortCaller {
         heapSort.heapSort(arr);
 
         System.out.println("****Array after sorting*****");
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(arr[i] + " ");
+        for(int i=0;i<arr.size();i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+
+
+        System.out.println("\n**after inserting 16 into heap***");
+
+        for (int i = arr.size() / 2 - 1; i >= 0; i--) {
+            heapSort.heapify(arr, arr.size(), i);
+        }
+
+        heapSort.insertNode(arr,16);
+
+        for(int i=0;i<arr.size();i++) {
+            System.out.print(arr.get(i) + " ");
         }
 
     }
